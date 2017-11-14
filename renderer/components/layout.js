6ada12f551636ef94410fee92320e0a34aabcb40
Andy Bitz <artzbitz13+git@gmail.com>
2017-11-14 12:34:41 +0100

    Show in column direction

--- renderer/components/layout.js
+++ renderer/components/layout.js
@@ -23,6 +23,7 @@ export default ({children}) => (
         width: 100vw;
   
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
       }
