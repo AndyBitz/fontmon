f09deb82a537ede453ec2a3626ad8411c10d26f1
Andy Bitz <artzbitz@gmail.com>
2017-11-15 13:44:01 +0100

    Styled loading screen

--- renderer/components/loading-overlay.js
+++ renderer/components/loading-overlay.js
@@ -1,21 +1,26 @@
 export default ({ isLoading }) => (
   <section className={isLoading ? 'visible' : ''} >
-    <div>loading ...</div>
+    <div>loading</div>
+    <Bar />
     <style jsx>
     {`
       section {
         position: fixed;
+        flex-direction: column;
 
         top: 0;
         bottom: 0;
         right: 0;
         left: 0;
 
-        background-color: red;
+        background-color: rgba(0, 0, 157, .7);
+        color: white;
 
         display: flex;
         justify-content: center;
         align-items: center;
+
+        transition: all 200ms ease-in-out;
 
         pointer-events: none;
         opacity: 0;
@@ -28,4 +33,49 @@ export default ({ isLoading }) => (
     `}
     </style>
   </section>
+)
+
+const Bar = () => (
+  <div>
+    <style jsx>
+    {`
+      div {
+        margin-top: .2em;
+        border: 3px solid white;
+        border-bottom: none;
+        border-top: none;
+        width: 10em;
+        height: .4em;
+        background: none;
+        position: relative;
+      }
+
+      div::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 48%;
+        right: 48%;
+        background-color: white;
+        animation: animat-bar 1.2s ease-in-out infinite;
+      }
+
+      @keyframes animat-bar {
+        0% {
+          right: 48%;
+          left: 48%;
+        }
+        50% {
+          right: -5%;
+          left: -5%;
+        }
+        100% {
+          right: 48%;
+          left: 48%;
+        }
+      }
+    `}
+    </style>
+  </div>
 )
